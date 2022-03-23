b3fa1675b965b4c22baf25367eeb3dda9b303d58
debrajNoi <101630180+debrajNoi@users.noreply.github.com>
2022-03-23 17:42:05 +0530

    client

--- src/constant.js
+++ src/constant.js
@@ -1,9 +1,24 @@
 // Constants.js
+// const prod = {
+//  url: {
+//   API_URL: 'http://localhost:8000/AllphanesuserAdd'
+//  }
+// }
+
+// const dev = {
+//  url: {
+//   API_URL: 'http://localhost:8000/AllphanesuserAdd'
+//  }
+// }
+// export const config = process.env.NODE_ENV === 'development' ? dev : prod
+
 const prod = {
  url: {
-  API_URL: 'http://localhost:8000/AllphanesuserAdd'
+  API_URL: 'https://powerful-shelf-35750.herokuapp.com/AllphanesuserAdd/'
+  API_URL: 'https://allphanesusernode.herokuapp.com/Allphanesuserpost'
  }
 }
+
 
 const dev = {
  url: {
